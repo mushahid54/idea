aa280b85436f0b2fc6bec1a64e991a987234902d
Mushahid <mushahidcs0054@gmail.com>
2017-03-13 17:38:45 +0530

    update static files path in the project

--- solar/settings.py
+++ solar/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 # STATIC_ROOT = str(BASE_DIR / 'solar' / 'staticfiles')
 GRAPPELLI_ADMIN_TITLE = "OORJAN BACKEND ADMIN"
 
-STATIC_ROOT = str(BASE_DIR / 'solar' / 'staticfiles')
+STATIC_ROOT = str(BASE_DIR / 'oorjan-task' / 'staticfiles')
 
 STATIC_URL = '/static/'
 
